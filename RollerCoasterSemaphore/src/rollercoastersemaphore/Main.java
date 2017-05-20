8c67f6e660b92cd7c5af381780f0a076518d4fac
Breno Viana <brenomfviana@gmail.com>
2017-05-19 22:32:52 -0300

    Creates two projects each using different concurrent programming methods.

---
+++ RollerCoasterSemaphore/src/rollercoastersemaphore/Main.java
@@ -0,0 +1,19 @@
+/*
+ * GNU License.
+ */
+package rollercoastersemaphore;
+
+/**
+ *
+ * @author breno
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
