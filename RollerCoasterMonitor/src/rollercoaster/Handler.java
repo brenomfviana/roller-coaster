893bfdb64d3af8fe866109aaf777ae16731c2edd
Breno Viana <brenomfviana@gmail.com>
2017-05-20 16:19:15 -0300

    Starts the implementation of roller coaster handler.

--- RollerCoasterMonitor/src/rollercoaster/Handler.java
+++ RollerCoasterMonitor/src/rollercoaster/Handler.java
@@ -3,18 +3,56 @@
  */
 package rollercoaster;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 /**
  * Roller Coaster handler.
  *
  * @author Breno Viana
- * @version 17/05/2017
+ * @version 20/05/2017
  */
 public class Handler {
 
+    // Roller Coaster car
+    private static Car car = Car.getInstance();
+
     /**
-     * .
+     * Run.
      */
     public static void run() {
-        // 
+        // Passengers
+        List<Passenger> passengers = new ArrayList<>();
+        // Creates the passengers
+        for (int i = 0; i < ((new Random()).nextInt(1) + 2); i++) {
+            passengers.add(new Passenger(i + 1));
+        }
+        // Runs passengers
+        passengers.stream().forEach((Passenger p) -> p.start());
+        // Simulation
+        while (true) {
+            // Unload
+            if (car.isStopped() && car.isFull()) {
+                // Allow unboarding
+                car.unload();
+            }
+            // Check if the car will still work
+            if (!car.isWorking()) {
+                break;
+            }
+            // Check if the car is empty
+            if (car.isStopped() && car.isEmpty()) {
+                // signal
+                // Allow boarding
+                car.load();
+            }
+            // Check if the car is ready
+            if (car.isStopped() && car.isFull() && !car.isAllowUnboarding()) {
+                // wait
+                // Run the ride
+                car.run();
+            }
+        }
     }
 }
--- RollerCoasterMonitor/src/rollercoaster/Main.java
+++ RollerCoasterMonitor/src/rollercoaster/Main.java
@@ -7,7 +7,7 @@ package rollercoaster;
  * This is the main class of Roller Coaster.
  *
  * @author Breno Viana
- * @version 17/05/2017
+ * @version 20/05/2017
  */
 public class Main {
 
@@ -15,6 +15,6 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Handler.run();
     }
 }
